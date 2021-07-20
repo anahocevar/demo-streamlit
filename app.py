0e2760a86f6323d991cd6f942cd3ae86b237dae2
anahocevar <ahocevara@gmail.com>
2021-07-20 19:35:22 +0200

    demonstrated simple streamlit features

--- app.py
+++ app.py
@@ -1,60 +1,29 @@
 import streamlit as st
-import pandas as pd
-from vega_datasets import data
-import altair as alt
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 def app():
     st.title('This is a demo')
+
+    st.markdown("""I will show you a couple of things!""")
+    st.markdown("""
+    * thing 1
+    * thing 2""")
     
-    st.markdown(""" ## This is some section
-    What else:
-    * this
-    * and this
+    st.sidebar.markdown("This is a sidebar!")
     
-    """)
+    N = int(st.text_input("How many points?", '5'))
     
-    st.sidebar.markdown('''Here is the sidebar text''')
+    fig, ax = plt.subplots()
+    x = np.random.random(N)*10
+    y = np.random.random(N)*10
+    plt.scatter(x, y, s=20)
+
+    st.pyplot(fig)
     
-    df_cars = data.cars()
-    st.write(df_cars.head())
-    
-    brush = alt.selection_interval(encodings=['x', 'y'])
-    repeat_chart = alt.Chart(df_cars).mark_point().encode(
-    alt.X(alt.repeat('column'), type='quantitative'),
-    alt.Y('Miles_per_Gallon:Q'),
-    color=alt.condition(brush, 'Origin:N', alt.value('lightgray')),
-    opacity=alt.condition(brush, alt.value(0.7), alt.value(0.1))
-       ).properties(
-       width=150,
-       height=150
-       ).add_selection(
-           brush
-       ).repeat(
-       column=['Weight_in_lbs', 'Acceleration', 'Horsepower']
-        )
-    
-    st.write(repeat_chart)
-    
-    st.markdown('## User Input Demo')
-    
-    n = int(st.text_input('How many points do you want:', '50'))
-    
-    x = np.random.random(n) * 10
-    y = np.random.random(n) * 10
-    s = np.random.random(n)
-    
-    df = pd.DataFrame({'x':x, 'y':y, 'size': s})
-    
-    chart = alt.Chart(df, width=400,
-                      height=400).mark_point().encode(
-                  x='x',
-                  y='y',
-                  size=alt.Size('size', legend=None),
-                  tooltip=['size']
-                  ).interactive()
-    
-    st.write(chart)
+    df = pd.DataFrame({'a':[2,3,4], 'b': [3,1,5]})
+    st.write(df)
     
 if __name__ == '__main__':
     app()
